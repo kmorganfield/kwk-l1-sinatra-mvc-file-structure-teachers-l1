8619174bb5cfaf61257053851ffa5d2bf8ed0320
Kennedy Morganfield <kennedymorganfield@gmail.com>
2018-08-02 15:48:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,16 @@ class ApplicationController < Sinatra::Base
   	set :public_dir, "public"
   end
 
-  get "/" do
+  get "/" do #this is your default/homepage
   	erb :index
   end
+  
+  
+  
+  # get "/" do                                                                                                                                                                                                                           
+  #   @the_fortune = get_fortune
+  #   @the_user=params[:user]
+  #   return erb :results
+  # end
+  
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,8 @@
-class Model
-  # Replace with Dog clas
+def get_fortune
+  
+  fortune_array = ["You will win a free vacation", "You will meet the love of your life", "You will find a $20 bill on the ground", "You will sleep in"]
+  
+  return fortune_array.sample
 end
+
+puts get_fortune
